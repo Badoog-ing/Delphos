7d0a231f5bf2fb9964b8434a126af828da671cd6
Fernando Orellana <fernando.orellana@virginiogomez.cl>
2020-07-15 23:31:14 -0400

    Cambios de Venta incompletos

--- Delphos/Areas/Caja/Controllers/VentaController.cs
+++ Delphos/Areas/Caja/Controllers/VentaController.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.UI.HtmlControls;
+using System.Data.Entity;
 
 namespace Delphos.Areas.Caja.Controllers
 {
@@ -26,20 +28,25 @@ namespace Delphos.Areas.Caja.Controllers
             return View();
         }
 
+
         [HttpPost]
         public ActionResult Buscar(string searchString)
         {            
             _db = new bdSupermercado();
-            Producto p = new Producto();
+            
+
             var productos = from b in _db.Productos
                             select b;
 
             if (!string.IsNullOrEmpty(searchString))
             {
                 productos = productos.Where(b => b.Sku.ToString().Contains(searchString));
+                
             }
 
             return View(productos);
+
+
         }
     }
 }
