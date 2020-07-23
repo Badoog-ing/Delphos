4449f4b93a83335bf76391c23be4c6b7befa17a9
Badoog <pipe777@gmail.com>
2020-07-22 22:20:40 -0400

    models bodega

---
+++ Delphos/Models/Traspaso.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Delphos.Models
+{
+    public class Traspaso
+    {
+        public int Id { get; set; }
+        public DateTime FechaTraspaso { get; set; }
+        public string BodegaOrigen { get; set; }
+        public string BodegaDestino { get; set; }
+
+    }
+}
