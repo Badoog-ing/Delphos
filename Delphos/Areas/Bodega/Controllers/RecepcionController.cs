bee248e30f70961dd5dd0a66ad5838baa08e80a8
Badoog <pipe777@gmail.com>
2020-07-23 22:56:33 -0400

    gfsdfgd

--- Delphos/Areas/Bodega/Controllers/RecepcionController.cs
+++ Delphos/Areas/Bodega/Controllers/RecepcionController.cs
@@ -10,31 +10,9 @@ namespace Delphos.Areas.Bodega.Controllers
 {
     public class RecepcionController : Controller
     {
-        private bdSupermercado _db;
-        public ActionResult Index(string searchString)
+        bdSupermercado _db = new bdSupermercado();
+        public ActionResult Index()
         {
-            /*            _db = new bdSupermercado();
-                        Recepcion p = new Recepcion();
-                        var recepciones = from b in _db.Recepciones
-                                        select b;
-
-                        if (!string.IsNullOrEmpty(searchString))
-                        {
-                            productos = productos.Where(b => b.Sku.ToString().Contains(searchString));
-                            foreach (var d in productos)
-                            {
-                                ViewBag.Id = d.Id;
-                                ViewBag.Nombre = d.Nombre;
-                                ViewBag.Sku = d.Sku;
-                                ViewBag.Precio = d.Precio;
-                            }
-                            if (productos.Count() == 0)
-                            {
-                                *//*falta mostrar un mensaje de que no se encontro el producto*//*
-                                return Content("El producto no se encuentra");
-                            }
-                        }
-                        return View(productos);*/
             return View();
         }
 
