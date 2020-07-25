b87b15f29b179bf01ec2143ae2b401857d6ebc7f
Badoog <pipe777@gmail.com>
2020-07-25 14:18:27 -0400

    e

--- Delphos/Scripts/Global.js
+++ Delphos/Scripts/Global.js
@@ -1,7 +1,37 @@
-﻿$(document).ready(function () {
+﻿/*remover mensaje de validacion*/
+$(document).ready(function () {
     $('.alert').click(function () {
         $(this).remove();
     });
 });
 
 
+/*mostrar pass*/
+
+function mostrarContrasena() {
+        var tipo = document.getElementById("pass");
+        if (tipo.type == "password") {
+    tipo.type = "text";
+        } else {
+    tipo.type = "password";
+        }
+    }
+
+/*busqueda rapida*/
+$(document).ready(function () {
+    function Contains(text_one, text_two) {
+        if (text_one.indexOf(text_two) != -1)
+            return true;
+    }
+        $("#Search").keyup(function () {
+            var searchText = $("#Search").val().toLowerCase();
+            $(".Search").each(function () {
+                if (!Contains($(this).text().toLowerCase(), searchText)) {
+    $(this).hide();
+                }
+                else {
+    $(this).show();
+                }
+            });
+        });
+    });
