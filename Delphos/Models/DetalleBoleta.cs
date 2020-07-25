37d71beb88365f486fd9266fd32e19d8e476236e
Badoog <pipe777@gmail.com>
2020-07-24 20:01:32 -0400

    detalleboleta

---
+++ Delphos/Models/DetalleBoleta.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Delphos.Models
+{
+    public class DetalleBoleta
+    {
+        public int Id { get; set; }
+        public int IdNumeroBoleta { get; set; }
+        public int IdProducto { get; set; }
+        public int Cantidad { get; set; }
+        public int Total { get; set; }
+    }
+}
--- Delphos/Models/bdSupermercado.cs
+++ Delphos/Models/bdSupermercado.cs
@@ -13,6 +13,7 @@ namespace Delphos.Models
         public DbSet<Producto> Productos { get; set; }
         public DbSet<Recepcion> Recepciones { get; set; }
         public DbSet<Venta> Ventas { get; set; }
+        public DbSet<DetalleBoleta> DetalleBoletas { get; set; }
         public DbSet<TablaBodega> Bodegas { get; set; }
         public DbSet<Proveedor> Proveedores { get; set; }
         public DbSet<OrdenCompra> OrdenCompras { get; set; }
